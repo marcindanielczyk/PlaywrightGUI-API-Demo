bba590b914c50c6a7a2522532f02c3133ce15b76
Marcin <marcindanielczyk@gmail.com>
2024-06-27 17:59:55 +0200

    refactored code and added unhappyPath test for registration

--- GADTests/tests/e2e/login.spec.ts
+++ GADTests/tests/e2e/login.spec.ts
@@ -3,20 +3,20 @@ import { test, expect } from 'playwright/test';
 test.describe('User login to GAD', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
+    await page.getByTestId('user-dropdown').hover();
+    await page.locator('#loginBtn').click();
   });
 
-  test('should log in with valid credentials', async ({ page }) => {
-    const userId = 'testEmail@testmail.com';
+  test('should log in with valid credentials', { tag: '@happyPath' }, async ({ page }) => {
+    const userEmail = 'testEmail@testmail.com';
     const userPassword = 'testPassword';
     const expectedUrl = 'http://localhost:3000/welcome';
 
-    await page.getByTestId('user-dropdown').hover();
-    await page.locator('#loginBtn').click();
-    await page.locator('#username').nth(1).fill(userId);
+    await page.locator('#username').nth(1).fill(userEmail);
     await page.locator('#password').fill(userPassword);
     await page.locator('#loginButton').click();
 
     await page.waitForURL(expectedUrl);
-    await expect(page.url()).toBe(expectedUrl);
+    expect(page.url()).toBe(expectedUrl);
   });
 });
--- GADTests/tests/e2e/registration.spec.ts
+++ GADTests/tests/e2e/registration.spec.ts
@@ -1,22 +1,28 @@
 import { test, expect } from 'playwright/test';
 
 test.describe('User registration to GAD', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/');
+  const registerUrl = 'http://localhost:3000/register.html';
+  const loginUrl = 'http://localhost:3000/login/';
+
+  test.beforeAll(async ({ request }) => {
+    const restoreDB = await request.get('/api/restoreDB');
+    expect(restoreDB.ok()).toBeTruthy();
   });
 
-  test('should register with valid credentials', async ({ page }) => {
-    const registerUrl = 'http://localhost:3000/register.html';
-    const loginUrl = 'http://localhost:3000/login/';
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+    await page.getByTestId('user-dropdown').hover();
+    await page.locator('#registerBtn').click();
+    expect(page.url()).toBe(registerUrl);
+  });
+
+  test('should register with valid credentials', { tag: '@happyPath' }, async ({ page }) => {
     const firstNameId = 'testName';
     const lastNameId = 'testLastName';
     const emailId = 'testEmail@testmail.com';
     const birthDateId = '2000-01-01';
     const passwordId = 'testPassword';
 
-    await page.getByTestId('user-dropdown').hover();
-    await page.locator('#registerBtn').click();
-    expect(page.url()).toBe(registerUrl);
     await page.getByTestId('firstname-input').fill(firstNameId);
     await page.getByTestId('lastname-input').fill(lastNameId);
     await page.getByTestId('email-input').fill(emailId);
@@ -26,8 +32,34 @@ test.describe('User registration to GAD', () => {
     await page.locator('#avatar').nth(1).selectOption({ index: 1 });
     await page.locator('#registerButton').click();
 
-    expect(page.url()).toBe(registerUrl);
     await page.waitForURL(loginUrl);
     expect(page.url()).toBe(loginUrl);
   });
+
+  test('should not register with email not unique', { tag: '@unhappyPath' }, async ({ page }) => {
+    const firstNameId = 'testName';
+    const lastNameId = 'testLastName';
+    const emailId = 'testEmail@testmail.com';
+    const birthDateId = '2000-01-01';
+    const passwordId = 'testPassword';
+
+    await page.getByTestId('firstname-input').fill(firstNameId);
+    await page.getByTestId('lastname-input').fill(lastNameId);
+    await page.getByTestId('email-input').fill(emailId);
+    await page.getByTestId('birthdate-input').fill(birthDateId);
+    await page.click('body');
+    await page.getByTestId('password-input').fill(passwordId);
+    await page.locator('#avatar').nth(1).selectOption({ index: 1 });
+    await page.locator('#registerButton').click();
+
+    await expect(page.getByTestId('alert-popup')).toHaveText('User not created! Email not unique');
+  });
+
+  // test('should not register without email', { tag: '@unhappyPath' }, async ({ page }) => {});
+
+  // test('should not register without firstname', { tag: '@unhappyPath' }, async ({ page }) => {});
+
+  // test('should not register without lastname', { tag: '@unhappyPath' }, async ({ page }) => {});
+
+  // test('should not register without password', { tag: '@unhappyPath' }, async ({ page }) => {});
 });
