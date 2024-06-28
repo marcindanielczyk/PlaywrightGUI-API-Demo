ac8e0f488af18e7b2dcc4288a05f02ec75791a1b
Marcin <marcindanielczyk@gmail.com>
2024-06-28 15:19:42 +0200

    added test to test registering user without email

--- GADTests/tests/e2e/registration.spec.ts
+++ GADTests/tests/e2e/registration.spec.ts
@@ -16,46 +16,82 @@ test.describe('User registration to GAD', () => {
     expect(page.url()).toBe(registerUrl);
   });
 
-  test('should register with valid credentials', { tag: '@happyPath' }, async ({ page }) => {
-    const firstNameId = 'testName';
-    const lastNameId = 'testLastName';
-    const emailId = 'testEmail@testmail.com';
-    const birthDateId = '2000-01-01';
-    const passwordId = 'testPassword';
+  test(
+    'should register with valid credentials',
+    {
+      tag: '@happyPath',
+      annotation: { type: 'link', description: 'info about tested application -> https://jaktestowac.pl/about-gad/#Main_features' },
+    },
+    async ({ page }) => {
+      const firstNameId = 'testName';
+      const lastNameId = 'testLastName';
+      const emailId = 'testEmail@testmail.com';
+      const birthDateId = '2000-01-01';
+      const passwordId = 'testPassword';
 
-    await page.getByTestId('firstname-input').fill(firstNameId);
-    await page.getByTestId('lastname-input').fill(lastNameId);
-    await page.getByTestId('email-input').fill(emailId);
-    await page.getByTestId('birthdate-input').fill(birthDateId);
-    await page.click('body');
-    await page.getByTestId('password-input').fill(passwordId);
-    await page.locator('#avatar').nth(1).selectOption({ index: 1 });
-    await page.locator('#registerButton').click();
+      await page.getByTestId('firstname-input').fill(firstNameId);
+      await page.getByTestId('lastname-input').fill(lastNameId);
+      await page.getByTestId('email-input').fill(emailId);
+      await page.getByTestId('birthdate-input').fill(birthDateId);
+      await page.click('body');
+      await page.getByTestId('password-input').fill(passwordId);
+      await page.locator('#avatar').nth(1).selectOption({ index: 1 });
+      await page.locator('#registerButton').click();
 
-    await page.waitForURL(loginUrl);
-    expect(page.url()).toBe(loginUrl);
-  });
+      await page.waitForURL(loginUrl);
+      expect(page.url()).toBe(loginUrl);
+    },
+  );
 
-  test('should not register with email not unique', { tag: '@unhappyPath' }, async ({ page }) => {
-    const firstNameId = 'testName';
-    const lastNameId = 'testLastName';
-    const emailId = 'testEmail@testmail.com';
-    const birthDateId = '2000-01-01';
-    const passwordId = 'testPassword';
+  test(
+    'should not register with email not unique',
+    {
+      tag: '@unhappyPath',
+      annotation: { type: 'link', description: 'info about tested application -> https://jaktestowac.pl/about-gad/#Main_features' },
+    },
+    async ({ page }) => {
+      const firstNameId = 'testName';
+      const lastNameId = 'testLastName';
+      const emailId = 'testEmail@testmail.com';
+      const birthDateId = '2000-01-01';
+      const passwordId = 'testPassword';
 
-    await page.getByTestId('firstname-input').fill(firstNameId);
-    await page.getByTestId('lastname-input').fill(lastNameId);
-    await page.getByTestId('email-input').fill(emailId);
-    await page.getByTestId('birthdate-input').fill(birthDateId);
-    await page.click('body');
-    await page.getByTestId('password-input').fill(passwordId);
-    await page.locator('#avatar').nth(1).selectOption({ index: 1 });
-    await page.locator('#registerButton').click();
+      await page.getByTestId('firstname-input').fill(firstNameId);
+      await page.getByTestId('lastname-input').fill(lastNameId);
+      await page.getByTestId('email-input').fill(emailId);
+      await page.getByTestId('birthdate-input').fill(birthDateId);
+      await page.click('body');
+      await page.getByTestId('password-input').fill(passwordId);
+      await page.locator('#avatar').nth(1).selectOption({ index: 1 });
+      await page.locator('#registerButton').click();
 
-    await expect(page.getByTestId('alert-popup')).toHaveText('User not created! Email not unique');
-  });
+      await expect(page.getByTestId('alert-popup')).toHaveText('User not created! Email not unique');
+    },
+  );
 
-  // test('should not register without email', { tag: '@unhappyPath' }, async ({ page }) => {});
+  test(
+    'should not register without email',
+    {
+      tag: '@unhappyPath',
+      annotation: { type: 'link', description: 'info about tested application -> https://jaktestowac.pl/about-gad/#Main_features' },
+    },
+    async ({ page }) => {
+      const firstNameId = 'testName';
+      const lastNameId = 'testLastName';
+      const birthDateId = '2000-01-01';
+      const passwordId = 'testPassword';
+
+      await page.getByTestId('firstname-input').fill(firstNameId);
+      await page.getByTestId('lastname-input').fill(lastNameId);
+      await page.getByTestId('birthdate-input').fill(birthDateId);
+      await page.click('body');
+      await page.getByTestId('password-input').fill(passwordId);
+      await page.locator('#avatar').nth(1).selectOption({ index: 1 });
+      await page.locator('#registerButton').click();
+
+      await expect(page.locator('#octavalidate_email')).toHaveText('This field is required');
+    },
+  );
 
   // test('should not register without firstname', { tag: '@unhappyPath' }, async ({ page }) => {});
 
