3bd5e00a07bd217773ef311b202243934b35332b
Marcin <marcindanielczyk@gmail.com>
2024-07-03 17:21:48 +0200

    add user should not log in if does not exist test

--- GADTests/tests/e2e/login.spec.ts
+++ GADTests/tests/e2e/login.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from 'playwright/test';
 test.describe('User login to GAD', () => {
   const firstNameId = 'testName';
   const lastNameId = 'testLastName';
-  const emailId = 'okokokok@testmail.com';
+  const emailId = 'test@testmail.com';
   const birthDateId = '2000-01-01';
   const passwordId = 'testPassword';
   const avatarId = '.\\data\\users\\16709f41-acf3-4738-b4f8-0616a6b6e7ae.jpg';
@@ -31,13 +31,8 @@ test.describe('User login to GAD', () => {
     expect(createNewUser.ok()).toBeTruthy();
   });
 
-  test('user should log in with valid credentials', { tag: '@happyPath' }, async ({ page, request }) => {
+  test('user should log in with valid credentials', { tag: ['@happyPath', '@databaseDependent'] }, async ({ page, request }) => {
     const welcomeUrl = 'http://localhost:3000/welcome';
-
-    // w wypadku takiego dodania uzytkownika i tak przy kolejnym puszczeniu testu, ten uzytkownik juz istnieje wiec jest konflikt, stworzony uzytkownik
-    // czyli no zostaje ten restoreDB, prawdziwy mock albo stworzyenie uzytkownika w beforeEach/beforeAll i usuniecie go po wykonaniu testow afterall/aftereach
-    // no jeszcze moze byc usuniecie go tylko po tym tescie czy jakos tak
-    // tez teoretycznie tego fakera mozna sprobowac no ale nwm
 
     await page.locator('input#username').fill(emailId);
     await page.locator('#password').fill(passwordId);
@@ -47,12 +42,24 @@ test.describe('User login to GAD', () => {
     expect(page.url()).toBe(welcomeUrl);
   });
 
-  // mockowanie jest stabilniejsze i szybsze, ale trudniejsze
-  // dodanie usera bezposrednio w teście jest łatwiejsze
-  // pytanie brzmi czy w testach e2e mam uzywać łączenia api z gui? (w sumie w registration juz uzylem api do restora bazy to nie wiem czy tak robic)
-  // wiec tez w registration przy testach databasedependent zmienic na mocka, albo utworzyć bezposrednio w kodzie usera przy pomocy api(?) (lepij gui(?) czy nie)
+  test('user should not log in if does not exist', { tag: '@unhappyPath' }, async ({ page, request }) => {
+    const checkIfUserDoesExist = await request.get(`api/users?email=${emailId}`);
+    const body = await checkIfUserDoesExist.json();
+    console.log(body);
 
-  // test('user should not log in if does not exist', { tag: '@unhappyPath'}, async ({page}) => {});
+    if (body.length > 0) {
+      const userId = body[0].id;
+      const deleteUser = await request.delete(`api/users/${userId}`);
+      expect(deleteUser.ok()).toBeTruthy();
+    }
+
+    await page.locator('input#username').fill(emailId);
+    await page.locator('#password').fill(passwordId);
+    await page.locator('#loginButton').click();
+
+    const loginErrorMessage = await page.locator('login-error').innerText();
+    expect(loginErrorMessage).toBe('Invalid username or password');
+  });
 
   // user should not log in without email provided
 
