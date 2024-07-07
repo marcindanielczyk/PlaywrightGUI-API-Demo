19e4613301374da6d1545154f682f295e209e397
Marcin <marcindanielczyk@gmail.com>
2024-07-07 13:39:43 +0200

    added functions create and delete user for tests, added user should not log in without password provided test and changed user should not log in without email provided test, changed tags to relevant

--- GADTests/tests/e2e/login.spec.ts
+++ GADTests/tests/e2e/login.spec.ts
@@ -1,13 +1,12 @@
 import { test, expect } from 'playwright/test';
 import { v4 as UUID4 } from 'uuid';
+import { createUser, deleteUser } from '../../pages/login.page';
 
 test.describe('User login to GAD', () => {
-  const firstNameId = 'testName';
-  const lastNameId = 'testLastName';
-  const emailId = `test-${UUID4()}@example.com`;
-  const birthDateId = '2000-01-01';
-  const passwordId = 'testPassword';
-  const avatarId = '.\\data\\users\\16709f41-acf3-4738-b4f8-0616a6b6e7ae.jpg';
+  test.beforeAll(async ({ request }) => {
+    const restoreDB = await request.get('/api/restoreDB');
+    expect(restoreDB.ok()).toBeTruthy();
+  });
 
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
@@ -15,70 +14,57 @@ test.describe('User login to GAD', () => {
     await page.locator('#loginBtn').click();
   });
 
-  test.beforeAll(async ({ request }) => {
-    const restoreDB = await request.get('/api/restoreDB');
-    expect(restoreDB.ok()).toBeTruthy();
+  test('user should log in with valid credentials', { tag: '@happyPath' }, async ({ page, request }) => {
+    const email = `test-${UUID4()}@example.com`;
+    await createUser(request, email);
 
-    const createNewUser = await request.post('api/users', {
-      data: {
-        firstname: firstNameId,
-        lastname: lastNameId,
-        email: emailId,
-        password: passwordId,
-        avatar: avatarId,
-      },
-    });
-    expect(createNewUser.ok()).toBeTruthy();
+    const welcomeUrl = 'http://localhost:3000/welcome';
 
-    test('user should log in with valid credentials', { tag: ['@happyPath', '@databaseDependent'] }, async ({ page, request }) => {
-      const welcomeUrl = 'http://localhost:3000/welcome';
+    await page.locator('input#username').fill(email);
+    await page.locator('#password').fill('testPassword');
+    await page.locator('#loginButton').click();
 
-      await page.locator('input#username').fill(emailId);
-      await page.locator('#password').fill(passwordId);
-      await page.locator('#loginButton').click();
+    await page.waitForURL(welcomeUrl);
+    expect(page.url()).toBe(welcomeUrl);
 
-      await page.waitForURL(welcomeUrl);
-      expect(page.url()).toBe(welcomeUrl);
-    });
-
-    test(
-      'user should not log in if does not exist',
-      { tag: ['@unhappyPath', '@databaseDependent', '@workersDependent'] },
-      async ({ page, request }) => {
-        const checkIfUserDoesExist = await request.get(`api/users?email=${emailId}`);
-        const body = await checkIfUserDoesExist.json();
-        console.log(body);
-
-        if (body.length > 0) {
-          const loginToGetAccessToken = await request.post('api/login', {
-            data: {
-              email: emailId,
-              password: passwordId,
-            },
-          });
-          expect(loginToGetAccessToken.ok()).toBeTruthy();
-
-          const accessTokenBody = await loginToGetAccessToken.json();
-          const accessToken = accessTokenBody.access_token;
-
-          const userId = body[0].id;
-          const deleteUser = await request.delete(`api/users/${userId}`, {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          });
-          expect(deleteUser.ok()).toBeTruthy();
-        }
-
-        await page.locator('input#username').fill(emailId);
-        await page.locator('#password').fill(passwordId);
-        await page.locator('#loginButton').click();
-
-        const loginErrorMessage = await page.getByTestId('login-error').innerText();
-        expect(loginErrorMessage).toBe('Invalid username or password');
-      },
-    );
-
-    test('user should not log in without login', { tag: '@unhappyPath' }, async ({ page }) => {});
+    await deleteUser(request, email);
   });
-}); 
+
+  test('user should not log in if does not exist', { tag: '@unhappyPath' }, async ({ page, request }) => {
+    const email = `test-${UUID4()}@example.com`;
+    await deleteUser(request, email);
+
+    await page.locator('input#username').fill(email);
+    await page.locator('#password').fill('testPassword');
+    await page.locator('#loginButton').click();
+
+    const loginErrorMessage = await page.getByTestId('login-error').innerText();
+    expect(loginErrorMessage).toBe('Invalid username or password');
+  });
+
+  test('user should not log in without email provided', { tag: '@unhappyPath' }, async ({ page, request }) => {
+    const email = `test-${UUID4()}@example.com`;
+    await createUser(request, email);
+
+    await page.locator('#password').fill('testPassword');
+    await page.locator('#loginButton').click();
+
+    const loginErrorMessage = await page.getByTestId('login-error').innerText();
+    expect(loginErrorMessage).toBe('Invalid username or password');
+
+    await deleteUser(request, email);
+  });
+
+  test('user should not log in without password provided', { tag: '@unhappyPath' }, async ({ page, request }) => {
+    const email = `test-${UUID4()}@example.com`;
+    await createUser(request, email);
+
+    await page.locator('input#username').fill(email);
+    await page.locator('#loginButton').click();
+
+    const loginErrorMessage = await page.getByTestId('login-error').innerText();
+    expect(loginErrorMessage).toBe('Invalid username or password');
+
+    await deleteUser(request, email);
+  });
+});
