f0dc95afbef3aad9430734b8f07ae2e19f4c10e8
Marcin <marcindanielczyk@gmail.com>
2024-07-09 16:09:53 +0200

    add beforeEach function to articles tests

--- GADTests/tests/e2e/articles.spec.ts
+++ GADTests/tests/e2e/articles.spec.ts
@@ -5,4 +5,13 @@ test.describe('Add articles to GAD', () => {
     const restoreDB = await request.get('/api/restoreDB');
     expect(restoreDB.ok()).toBeTruthy();
   });
+
+  test.beforeEach(async ({ page }) => {
+    const articlesUrl = 'http://localhost:3000/articles.html';
+
+    await page.goto('/');
+    await page.locator('#btnGui').click();
+
+    expect(page.url()).toBe(articlesUrl);
+  });
 });
