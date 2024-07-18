2bc127469b188aae61fe9e3361fcb642b95e2801
Marcin <marcindanielczyk@gmail.com>
2024-07-18 15:01:12 +0200

    minor changes in variables and whitespaces

--- GADTests/helpers/users/deleteUserIfExists.ts
+++ GADTests/helpers/users/deleteUserIfExists.ts
@@ -1,19 +1,18 @@
 import { expect } from 'playwright/test';
 
-
 export async function deleteUserIfExists(request, email) {
-    const getUserByEmail = await request.get(`api/users?email=${email}`);
-    const body = await getUserByEmail.json();
-    if (body.length > 0) {
-      const loginToGetAccessToken = await request.post('api/login', {
-        data: { email: email, password: 'testPassword' },
-      });
-      const accessTokenBody = await loginToGetAccessToken.json();
-      const accessToken = accessTokenBody.access_token;
-      const userId = body[0].id;
-      const deleteUser = await request.delete(`api/users/${userId}`, {
-        headers: { Authorization: `Bearer ${accessToken}` },
-      });
-      expect(deleteUser.ok()).toBeTruthy();
-    }
+  const getUserByEmail = await request.get(`api/users?email=${email}`);
+  const body = await getUserByEmail.json();
+  if (body.length > 0) {
+    const loginToGetAccessToken = await request.post('api/login', {
+      data: { email: email, password: 'testPassword' },
+    });
+    const loginBody = await loginToGetAccessToken.json();
+    const accessToken = loginBody.access_token;
+    const userId = body[0].id;
+    const deleteUser = await request.delete(`api/users/${userId}`, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+    expect(deleteUser.ok()).toBeTruthy();
   }
+}
