c5bcbd394f7c98085fe287efde1c89252ad54fab
Marcin <marcindanielczyk@gmail.com>
2024-07-27 14:44:51 +0200

    add delete article test and changed variable names to be more clear and accurate

--- GADTests/tests/e2e/articles.spec.ts
+++ GADTests/tests/e2e/articles.spec.ts
@@ -4,51 +4,47 @@ import { logInAsUserWithGUI } from '../../helpers/users/logInAsUserWithGUI.helpe
 import { deleteUserIfExists } from '../../helpers/users/deleteUserIfExists.helpers';
 import { deleteArticleIfExists } from '../../helpers/articles/deleteArticleIfExists.helpers';
 
-test.describe('Test articles with user logged in to GAD, then delete article with the same user', () => {
-  const email = `test-${UUID4()}@example.com`;
+test.describe('Articles CRUD operations with authenticated user', () => {
+  const testUserEmail = `test-${UUID4()}@example.com`;
 
   test.beforeAll(async ({ request }) => {
-    const restoreDB = await request.get('/api/restoreDB');
-    expect(restoreDB.ok()).toBeTruthy();
+    const dbRestoreResponse = await request.get('/api/restoreDB');
+    expect(dbRestoreResponse.ok()).toBeTruthy();
   });
 
   test.beforeEach(async ({ page, request }) => {
-    await logInAsUserWithGUI(page, request, email);
+    await logInAsUserWithGUI(page, request, testUserEmail);
 
-    const articlesUrl = 'http://localhost:3000/articles.html';
-
+    const articlesPageUrl = 'http://localhost:3000/articles.html';
     await page.getByTestId('open-articles').click();
-
-    expect(page.url()).toBe(articlesUrl);
+    expect(page.url()).toBe(articlesPageUrl);
   });
 
   test.afterEach(async ({ request }) => {
-    await deleteUserIfExists(request, email);
+    await deleteUserIfExists(request, testUserEmail);
   });
 
-  test('create article with user logged in', { tag: ['@happyPath', '@flaky'], annotation: { type: 'info', description: 'flaky when run with 2 workers, because of beforeAll restoreDB race condition' } }, async ({ page, request }) => {
+  test('create article with authenticated user', { tag: ['@happyPath', '@flaky'], annotation: { type: 'info', description: 'flaky when run with 2 workers, because of beforeAll restoreDB race condition' } }, async ({ page, request }) => {
     await page.locator('#add-new').click();
     await page.getByTestId('title-input').fill('testTitle');
     await page.getByTestId('body-text').fill('testBody');
 
-    const responsePromise = page.waitForResponse((response) => response.url().includes('/api/articles') && response.status() === 201);
-
+    const createArticleResponsePromise = page.waitForResponse((response) => response.url().includes('/api/articles') && response.status() === 201);
     await page.getByTestId('save').click();
+    const createArticleResponse = await createArticleResponsePromise;
 
-    const response = await responsePromise;
-
-    const headers = response.headers();
-    const articleLocation = headers['location'];
-    const articleId = articleLocation.split('/').pop();
+    const responseHeaders = createArticleResponse.headers();
+    const newArticleLocation = responseHeaders['location'];
+    const newArticleId = newArticleLocation.split('/').pop();
 
     await page.getByTestId('alert-popup').waitFor({ state: 'visible' });
-    const alertText = await page.getByTestId('alert-popup').innerText();
-    expect(alertText).toBe('Article was created');
+    const createAlertText = await page.getByTestId('alert-popup').innerText();
+    expect(createAlertText).toBe('Article was created');
 
-    await deleteArticleIfExists(request, articleId, email, 'testPassword');
+    await deleteArticleIfExists(request, newArticleId, testUserEmail, 'testPassword');
   });
 
-  test('update article with user logged in', { tag: '@happyPath' }, async ({ page, request }) => {
+  test('update article with authenticated user', { tag: '@happyPath' }, async ({ page, request }) => {
     const updatedTitle = 'testTitleEdit';
     const updatedBody = 'testBodyEdit';
 
@@ -56,38 +52,64 @@ test.describe('Test articles with user logged in to GAD, then delete article wit
     await page.getByTestId('title-input').fill('testTitle');
     await page.getByTestId('body-text').fill('testBody');
 
-    const createResponsePromise = page.waitForResponse((response) => response.url().includes('/api/articles') && response.status() === 201);
-
+    const createArticleResponsePromise = page.waitForResponse((response) => response.url().includes('/api/articles') && response.status() === 201);
     await page.getByTestId('save').click();
+    const createArticleResponse = await createArticleResponsePromise;
 
-    const createResponse = await createResponsePromise;
-    const createHeaders = createResponse.headers();
-    const createLocation = createHeaders['location'];
-    const createArticleId = createLocation.split('/').pop();
+    const createResponseHeaders = createArticleResponse.headers();
+    const newArticleLocation = createResponseHeaders['location'];
+    const articleId = newArticleLocation.split('/').pop();
 
     await page.getByTestId('alert-popup').waitFor({ state: 'visible' });
-    const alertTextArticleCreated = await page.getByTestId('alert-popup').innerText();
-    expect(alertTextArticleCreated).toBe('Article was created');
+    const createAlertText = await page.getByTestId('alert-popup').innerText();
+    expect(createAlertText).toBe('Article was created');
 
     await page.getByTestId('edit').click();
     await page.getByTestId('title-input').fill(updatedTitle);
     await page.getByTestId('body-input').fill(updatedBody);
-    await page.getByTestId('update').click();
 
-    await page.waitForResponse(response => response.url().includes('/api/articles') && response.status() === 200);
+    const updateArticleResponsePromise = page.waitForResponse((response) => response.url().includes('/api/articles') && response.status() === 200);
+    await page.getByTestId('update').click();
+    await updateArticleResponsePromise;
 
     await page.getByTestId('alert-popup').waitFor({ state: 'visible' });
-    const alertTextArticleUpdated = await page.getByTestId('alert-popup').innerText();
-    expect(alertTextArticleUpdated).toBe('Article was updated');
+    const updateAlertText = await page.getByTestId('alert-popup').innerText();
+    expect(updateAlertText).toBe('Article was updated');
 
-    const updatedTitleValue = await page.getByTestId('article-title').innerText();
-    const updatedBodyValue = await page.getByTestId('article-body').innerText();
-    expect(updatedTitleValue).toBe(updatedTitle);
-    expect(updatedBodyValue).toBe(updatedBody);
+    const updatedTitleText = await page.getByTestId('article-title').innerText();
+    const updatedBodyText = await page.getByTestId('article-body').innerText();
+    expect(updatedTitleText).toBe(updatedTitle);
+    expect(updatedBodyText).toBe(updatedBody);
 
-    await deleteArticleIfExists(request, createArticleId, email, 'testPassword');
+    await deleteArticleIfExists(request, articleId, testUserEmail, 'testPassword');
   });
-  // test("delete article when user logged in")
-  // test("view article when user logged in")
-  // test("list articles when user logged in")
+
+  test('delete article with authenticated user', { tag: '@happyPath' }, async ({ page, request }) => {
+    await page.locator('#add-new').click();
+    await page.getByTestId('title-input').fill('testTitle');
+    await page.getByTestId('body-text').fill('testBody');
+
+    const createArticleResponsePromise = page.waitForResponse((response) => response.url().includes('/api/articles') && response.status() === 201);
+    await page.getByTestId('save').click();
+    const createArticleResponse = await createArticleResponsePromise;
+
+    const createResponseHeaders = createArticleResponse.headers();
+    const newArticleLocation = createResponseHeaders['location'];
+    const articleId = newArticleLocation.split('/').pop();
+
+    await page.getByTestId('alert-popup').waitFor({ state: 'visible' });
+    const createAlertText = await page.getByTestId('alert-popup').innerText();
+    expect(createAlertText).toBe('Article was created');
+
+    page.on('dialog', async (dialog) => {
+      expect(dialog.type()).toBe('confirm');
+      expect(dialog.message()).toContain('Are you sure that you want to delete item');
+      await dialog.accept();
+    });
+
+    const deleteArticleResponsePromise = page.waitForResponse((response) => response.url().includes('/api/articles') && response.status() === 200);
+    await page.getByTestId('delete').click();
+    const deleteArticleResponse = await deleteArticleResponsePromise;
+    expect(deleteArticleResponse.status()).toBe(200);
+  });
 });
