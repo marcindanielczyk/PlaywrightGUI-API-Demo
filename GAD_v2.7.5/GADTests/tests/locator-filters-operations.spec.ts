daba47b48072a7d536a655ec0444b61c72cec495
Marcin <marcindanielczyk@gmail.com>
2024-10-29 22:01:11 +0100

    add needed variables for Act section for Single button click using options

--- GAD_v2.7.5/GADTests/tests/locator-filters-operations.spec.ts
+++ GAD_v2.7.5/GADTests/tests/locator-filters-operations.spec.ts
@@ -1,46 +1,55 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test.describe("Locator filters", () => {
+test.describe('Locator filters', () => {
   // https://playwright.dev/docs/locators#locator-operators
 
   test.beforeEach(async ({ page }) => {
-    await page.goto("/practice/simple-multiple-elements-no-ids.html");
+    await page.goto('/practice/simple-multiple-elements-no-ids.html');
   });
 
-  test.describe("Finding element - different approaches", () => {
-    test("Single button click using options", async ({ page }) => {
+  test.describe('Finding element - different approaches', () => {
+    test('Single button click using options', async ({ page }) => {
       // Arrange:
+      const elementRole = 'button';
+      const resultsTestId = 'dti-results';
+      const expectedMessage = 'You clicked the button!';
+      const elementText = "Click me!";
+
+      const buttonLocator = page.getByRole(elementRole, { name : elementText });
+
       // Act:
+
+      
       // Assert:
     });
 
-    test("Single button click (using filter and hasText)", async ({ page }) => {
+    test('Single button click (using filter and hasText)', async ({ page }) => {
       // Arrange:
       // Act:
       // Assert:
     });
   });
 
-  test.describe("Buttons in table - different approaches", () => {
-    test("Single button click (chained getBy)", async ({ page }) => {
+  test.describe('Buttons in table - different approaches', () => {
+    test('Single button click (chained getBy)', async ({ page }) => {
       // Arrange:
       // Act:
       // Assert:
     });
 
-    test("Single button click (using filter and has)", async ({ page }) => {
+    test('Single button click (using filter and has)', async ({ page }) => {
       // Arrange:
       // Act:
       // Assert:
     });
 
-    test("Single button click (using filter and hasText)", async ({ page }) => {
+    test('Single button click (using filter and hasText)', async ({ page }) => {
       // Arrange:
       // Act:
       // Assert:
     });
 
-    test("Single button click (using filter by child)", async ({ page }) => {
+    test('Single button click (using filter by child)', async ({ page }) => {
       // Arrange:
       // Act:
       // Assert:
